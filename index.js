00bb3e67b1c4e6ca699ac6adc3054c55a2fcaa3d
DimaVax <126974388+DimaVax@users.noreply.github.com>
2024-05-18 20:50:17 +0200

    hm 1, version 1

---
+++ index.js
@@ -0,0 +1,50 @@
+"use strict";
+
+// 1
+const age = 12;
+console.log(age);
+
+// 2
+const myName = "Dima";
+console.log(myName);
+
+// 3
+const isStudent = false;
+console.log(isStudent);
+
+// 4
+const myString = "Найважливіше в житті не програмування, а програмування"
+console.log(myString);
+
+// 5
+let myNumber = 5;
+myNumber = myNumber + 10;
+console.log(myNumber);
+
+// 6
+const myNull = null;
+console.log(myNull);
+
+// 7
+// const message = 'Як тебе звати?';
+// const name = prompt(message);
+// console.log("Вітаю тебе,", name, ",з днем народження!");
+
+// 8
+// let answer = confirm("Ви хочете підтвердити це?");
+// if (answer === true) {
+//    alert("Дякую за підтвердження!");
+// }else {
+//    alert("Дію відмінено!");
+// }
+
+// 9
+const dangerous = alert("Дія небезпечна!");
+const answer = confirm("Ви хочете підтвердити дію?");
+if (answer === true) {
+   alert("Дякую за підтвердження!");
+}else {
+   alert("Дію відмінено!");
+}
+
+
